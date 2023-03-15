2640da3caaa9eb2355c304bebaf7e473f67338c5
adi0509 <adiyadav0509@gmail.com>
2023-03-15 15:51:25 +0530

    wait until container is in running status

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
- 
+
+@app.route('/')
+def hello_name():
+   return 'Hello World!'
+
 @app.route('/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
