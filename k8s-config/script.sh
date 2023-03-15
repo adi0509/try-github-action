d5deab43b5d3a32b2fac4ebd5eeb3ad2ad1f2865
adi0509 <adiyadav0509@gmail.com>
2023-03-15 21:03:39 +0530

    test rollout restart of deployment

--- k8s-config/script.sh
+++ k8s-config/script.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 kind create cluster --config=kind-config.yaml
+kubectl proxy --port=8000 --address 0.0.0.0 --accept-hosts '.*' &
 kubectl create serviceaccount github-actions
 kubectl apply -f clusterrole.yaml
 kubectl create clusterrolebinding continuous-deployment \
     --clusterrole=continuous-deployment --serviceaccount=default:github-actions
 # kubectl create token github-actions --duration=99999h
 kubectl create -f secret.yaml
-echo "\n\n-------------COPY THIS OUTPUT-------------"
+echo "\n\n-------------COPY THIS OUTPUT-------------\n"
 kubectl  get secrets github-action-token -o yaml
